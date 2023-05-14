5f51a0c0a7104da72799dc4ff2d12f1a68df672a
Acrata <hxtr227@gmail.com>
2023-05-14 22:36:45 +0200

    Fix main margin and position, refactor css

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,16 @@ import '../src/css/index.css'
 
 const nav = document.getElementById('main-nav')
 const navBtn = document.getElementById('nav-btn')
+const body = document.getElementsByTagName('body')[0]
 
 navBtn?.addEventListener('click', () => {
+  if (document.querySelectorAll('.open').length == 0) {
+    body.style.overflowY = 'hidden'
+    navBtn.setAttribute('aria-expanded', 'true')
+  } else {
+    body.style.overflowY = 'auto'
+    navBtn.setAttribute('aria-expanded', 'false')
+  }
   nav?.classList.toggle('open');
 })
 
